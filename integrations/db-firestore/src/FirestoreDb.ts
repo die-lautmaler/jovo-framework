f7a783d42d492a36e770816be4ac5decb0fa4905
Ruben Aegerter <email@aegerter.tech>
2022-05-24 12:47:18 +0200

    :sparkles: Add Firestore database integration

---
+++ integrations/db-firestore/src/FirestoreDb.ts
@@ -0,0 +1,112 @@
+import {
+  DbItem,
+  DbPlugin,
+  DbPluginConfig,
+  HandleRequest,
+  Jovo,
+  JovoError,
+  Logger,
+  RequiredOnlyWhere,
+} from '@jovotech/framework';
+import { CollectionReference, DocumentSnapshot, Firestore } from '@google-cloud/firestore';
+
+export interface FirestoreDbConfig extends DbPluginConfig {
+  collection: string;
+  credentials?: {
+    clientEmail: string;
+    privateKey: string;
+  };
+  keyFileName?: string;
+}
+
+// TODO: What do we actually need?
+export type FirestoreDbInitConfig = RequiredOnlyWhere<FirestoreDbConfig, 'collection'>;
+
+/** Default database name in MongoDB. if no name was specified, 'test' is used. See https://docs.mongodb.com/manual/tutorial/getting-started/#getting-started */
+export const JOVO_DEFAULT_COLLECTION_NAME = 'jovoUsers';
+
+export class FirestoreDb extends DbPlugin<FirestoreDbConfig> {
+  private readonly collection: CollectionReference;
+
+  constructor(config: FirestoreDbInitConfig) {
+    super(config);
+    this.collection = new Firestore({
+      keyFilename: this.config.keyFileName,
+      credentials: this.config.credentials
+        ? {
+            client_email: this.config.credentials.clientEmail,
+            private_key: this.config.credentials.privateKey,
+          }
+        : undefined,
+    }).collection(this.config.collection);
+  }
+
+  checkForCredentials(): void {
+    if (!process.env.GOOGLE_APPLICATION_CREDENTIALS && !this.config.keyFileName) {
+      if (this.config.credentials) {
+        if (!this.config.credentials.privateKey) {
+          throw new JovoError({
+            message: 'Credentials provided, but private key is missing',
+            hint: 'Provide privateKey in your database configuration',
+            package: '@lautmaler/db-firestore',
+          });
+        }
+
+        if (!this.config.credentials.clientEmail) {
+          throw new JovoError({
+            message: 'Credentials provided, but client email is missing',
+            hint: 'Provide clientEmail in your database configuration',
+            package: '@lautmaler/db-firestore',
+          });
+        }
+      } else {
+        throw new JovoError({
+          message: 'Credentials missing',
+          hint: 'Provide them through ...',
+          package: '@lautmaler/db-firestore',
+        });
+      }
+    }
+  }
+
+  getDefaultConfig(): FirestoreDbConfig {
+    return {
+      ...super.getDefaultConfig(),
+      collection: JOVO_DEFAULT_COLLECTION_NAME,
+    };
+  }
+
+  getInitConfig(): FirestoreDbInitConfig {
+    return { collection: '<YOUR-FIRESTOREB-COLLECTION-NAME>' };
+  }
+
+  mount(parent: HandleRequest): Promise<void> | void {
+    super.mount(parent);
+    parent.middlewareCollection.use('before.request.start', () => {
+      this.checkForCredentials();
+    });
+  }
+
+  async initialize(): Promise<void> {
+    if (this.config.collection === JOVO_DEFAULT_COLLECTION_NAME) {
+      Logger.warn(
+        `[FirestoreDb] Warning: The default collection "${JOVO_DEFAULT_COLLECTION_NAME}" name is being used.`,
+      );
+    }
+  }
+
+  async loadData(userId: string, jovo: Jovo): Promise<void> {
+    const document: DocumentSnapshot = await this.collection.doc(userId).get();
+    if (document.exists) {
+      jovo.$user.isNew = false;
+      // Use non-null assertion since we checked if the document exists already
+      jovo.setPersistableData(document.data()!, this.config.storedElements);
+    }
+  }
+
+  async saveData(userId: string, jovo: Jovo): Promise<void> {
+    const item: DbItem = { _id: userId };
+    await this.applyPersistableData(jovo, item);
+    await this.collection.doc(userId).set(item);
+  }
+}
---
+++ integrations/db-firestore/src/index.ts
@@ -0,0 +1,22 @@
+import { FirestoreDb, FirestoreDbConfig } from './FirestoreDb';
+// import { JovoFirestoreDb } from './JovoFirestoreDb';
+
+declare module '@jovotech/framework/dist/types/Extensible' {
+  interface ExtensiblePluginConfig {
+    FirestoreDb?: FirestoreDbConfig;
+  }
+
+  interface ExtensiblePlugins {
+    FirestoreDb?: FirestoreDb;
+  }
+}
+
+/*
+declare module '@jovotech/framework/dist/types/Jovo' {
+  interface Jovo {
+    $firestoreDb: JovoFirestoreDb;
+  }
+}
+*/
+
+export { FirestoreDb, FirestoreDbConfig };
